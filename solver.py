82751578a01f20704089ee869a45f6d5f8df0234
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2025-02-26 12:44:49 +0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -371,9 +371,9 @@ class Solver(object):
                 sal_loss.backward()
                 self.optimizer.step()
 
-                if (i + 1) % (self.show_every // self.config.batch_size) == 0:
+                '''if (i + 1) % (self.show_every // self.config.batch_size) == 0:
                     print('epoch: [%2d/%2d], iter: [%5d/%5d]  ||  Sal : %0.4f  ||sal_final:%0.4f|| edge_loss0:%0.4f' % (
-                        epoch, self.config.epoch, i + 1, iter_num, r_sal_loss,sal_final_loss,edge_loss_rgbd0 ))
+                        epoch, self.config.epoch, i + 1, iter_num, r_sal_loss,sal_final_loss,edge_loss_rgbd0 ))'''
                     # print('Learning rate: ' + str(self.lr))
                     '''writer.add_scalar('training loss', r_sal_loss / (self.show_every / self.iter_size),
                                       epoch * len(self.train_loader.dataset) + i)
