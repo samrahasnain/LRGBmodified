7125043bc859dac86b95981e396673c05af021b6
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2025-02-26 14:48:35 +0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -26,7 +26,7 @@ class ImageDataTrain(data.Dataset):
         im_name = self.sal_list[item % self.sal_num].split()[0]
         de_name = self.sal_list[item % self.sal_num].split()[0]
         gt_name = self.sal_list[item % self.sal_num].split()[1]
-        print(os.path.join(self.sal_root, im_name),os.path.join(self.sal_root, gt_name))
+        #print(os.path.join(self.sal_root, im_name),os.path.join(self.sal_root, gt_name))
         sal_image , im_size= load_image(os.path.join(self.sal_root, im_name), self.image_size)
         sal_depth, im_size = load_image(os.path.join(self.sal_root, de_name), self.image_size)
         sal_label,sal_edge = load_sal_label(os.path.join(self.sal_root, gt_name), self.image_size)
